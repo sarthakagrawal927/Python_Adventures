0fb9654611d0cb0ad17c8983c9605ac7cab249fe
Sarthak Agrawal <43884471+sarthakagrawal927@users.noreply.github.com>
2020-07-03 10:50:47 +0530

    practice python

--- Learn/scratch.py
+++ Learn/scratch.py
@@ -7,8 +7,9 @@ np_2D_array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 reshape_np_array = np_array.reshape(-1, 2)
 reshape_np_2D_array = np_2D_array.reshape(-1, 1)
 
-
-print(reshape_np_array)
-print(reshape_np_2D_array)
+greater_than5 = np_2D_array > 5
+print(np_2D_array[greater_than5])
+print(reshape_np_array.ndim)
+print(reshape_np_2D_array.ndim)
 print(np.arange(2, 12, 4))
 print(np.linspace(0, 20, num=5, dtype=np.int64))
