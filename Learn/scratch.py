fb74029165274faf264d430dbaa700163d50daa5
unknown <sarthakagrawal927@gmail.com>
2020-09-19 13:58:00 +0530

    ok sklearn idea done, time to dive in

--- Learn/scratch.py
+++ Learn/scratch.py
@@ -2,8 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 np_array = np.array([1, 2, 3, 4, 5, 6])
 
-personal_github_token = "570c93e665e2c2eb52efb16ec032ec11dc212d02"
-
 np_2D_array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
 reshape_np_array = np_array.reshape(-1, 2)
